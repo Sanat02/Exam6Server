6824b83df9f54833b35afc266e13fcff1bbe0b6e
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-07-01 10:47:45 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+//        try {
+//            new Lesson47Server("localhost", 8187).start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
